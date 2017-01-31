40dd10f85e8dc783f507eb2f401a9ec4d1cb488c
Pierre Marot <marot.pierre@gmail.com>
2017-01-31 12:04:36 +0100

    add evolve and decide

--- src/ddd/uno/domain/Domain.ts
+++ src/ddd/uno/domain/Domain.ts
@@ -2,18 +2,43 @@
  * Created by krazar on 31/01/2017.
  */
 
+import { curry } from 'ramda';
+import { List } from 'immutable';
 
 enum Color {
   Red, Green, Blue, Yellow,
 }
 
-
 enum Digit {
   Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine
 }
 
-
-
-enum Card {
-  Dig
+interface Card {
+  digit: Digit;
+  color: Color;
 }
+
+/** todo move to event*/
+interface SomethingHappened {
+  kind: 'SomethingHappened';
+}
+
+/** todo move to command*/
+class DoSomething {}
+class DoThat {}
+
+/** state */
+class InitinalState {}
+
+
+type UnoEvent =
+     SomethingHappened;
+
+type UnoCommand = DoSomething | DoThat;
+
+type State = InitinalState;
+
+
+let decide = curry((command: UnoCommand, state: State): UnoEvent[] => []);
+
+let evolve = curry((state: State, event: UnoEvent) => state);
