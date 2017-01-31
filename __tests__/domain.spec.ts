35c40e403f5f25b64aecda9e30767e9514c7e739
Pierre Marot <marot.pierre@gmail.com>
2017-01-31 16:27:12 +0100

    a lot of tests later

--- __tests__/domain.spec.ts
+++ __tests__/domain.spec.ts
@@ -1,14 +1,228 @@
 /**
  * Created by krazar on 31/01/2017.
  */
-import { decide, Color, Digit, InitinalState, StartGame } from '../src/ddd/uno/domain/Domain';
+import { decide, Color, Digit, evolve, UnoEvent, UnoCommand } from '../src/ddd/uno/domain/Domain';
 
-it('delays the greeting by 2 seconds', () => {
+const initState = {
+  kind: 'InitialState',
+};
 
-  const command: StartGame = {
-    kind: 'StartGame',
-    numberOfPlayer: 3,
-    firstCard: { digit: Digit.Five, color: Color.Blue },
-  };
-  expect(decide(command, InitinalState)).toEqual([{kind: 'GameStarted'}]);
+const given = (events: UnoEvent[], command: UnoCommand) => {
+  const state = events.reduce(evolve, initState);
+  return decide(command, state);
+};
+
+const expectResult = (result: UnoEvent[]) => (expected) => {
+  it('should equal expected', () => {
+    expect(result).toEqual(expected);
+  });
+};
+
+const expectFailure = (result: UnoEvent[], expected) => {
+  it('should be expected error', () => {
+    expect(result).toThrowError(expected);
+  });
+};
+
+const expectEvent = (events: UnoEvent[], command: UnoCommand, expected) => expectResult(given(events, command))(expected);
+const expectError = (events: UnoEvent[], command: UnoCommand, expected) => () => (expectFailure(given(events, command), expected));
+
+describe('as Initial State. when  StartGame Command is reieved', () => {
+
+  expectEvent([], {
+      kind: 'StartGame',
+      numberOfPlayer: 3,
+      firstCard: { digit: Digit.Five, color: Color.Blue },
+    },
+    [{
+      kind: 'GameStarted',
+      numberOfPlayer: 3,
+      firstCard: { digit: Digit.Five, color: Color.Blue },
+    }]);
+});
+
+describe('as game started state, when recieving StartGame Command', () => {
+
+  expectError(
+    [{
+      kind: 'GameStarted',
+      numberOfPlayer: 3,
+      firstCard: { digit: Digit.Five, color: Color.Blue },
+    }],
+    {
+      kind: 'StartGame',
+      numberOfPlayer: 4,
+      firstCard: { digit: Digit.Three, color: Color.Blue },
+    },
+    Error(),
+  );
+
+});
+
+describe('Playing same color should be ok', () => {
+
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      }],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Four, color: Color.Blue },
+      player: 1,
+    },
+    [
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Four, color: Color.Blue },
+        player: 1,
+      },
+    ],
+  );
+
+});
+
+describe('Playing some value should be ok', () => {
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Five, color: Color.Red },
+      player: 1,
+    },
+    [
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Five, color: Color.Red },
+        player: 1,
+      },
+    ],
+  );
+});
+
+describe('Player different color and different value should e rejected', () => {
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.One, color: Color.Red },
+      player: 1,
+    },
+    [
+      {
+        kind: 'InvalidCardPlayed',
+        card: { digit: Digit.One, color: Color.Red },
+        player: 1,
+      },
+    ],
+  );
+});
+
+describe(' Player can play at his turn', () => {
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Five, color: Color.Red },
+      player: 1,
+    },
+    [
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Five, color: Color.Red },
+        player: 1,
+      },
+    ],
+  );
+
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Five, color: Color.Red },
+        player: 1,
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Six, color: Color.Red },
+      player: 2,
+    },
+    [
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Six, color: Color.Red },
+        player: 2,
+      },
+    ],
+  );
+});
+
+describe('Player cannot play outside of his turn', () => {
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Five, color: Color.Red },
+      player: 2,
+    },
+    [
+      {
+        kind: 'NotPlayerTurn',
+        card: { digit: Digit.Five, color: Color.Red },
+        player: 2,
+      },
+    ],
+  );
+
+  expectEvent([
+      {
+        kind: 'GameStarted',
+        numberOfPlayer: 3,
+        firstCard: { digit: Digit.Five, color: Color.Blue },
+      },
+      {
+        kind: 'CardPlayed',
+        card: { digit: Digit.Five, color: Color.Red },
+        player: 1,
+      },
+    ],
+    {
+      kind: 'PlayCard',
+      card: { digit: Digit.Six, color: Color.Red },
+      player: 1,
+    },
+    [
+      {
+        kind: 'NotPlayerTurn',
+        card: { digit: Digit.Six, color: Color.Red },
+        player: 1,
+      },
+    ],
+  );
 });
--- src/ddd/uno/domain/Domain.ts
+++ src/ddd/uno/domain/Domain.ts
@@ -3,7 +3,6 @@
  */
 
 import { curry } from 'ramda';
-import { List } from 'immutable';
 
 export enum Color {
   Red, Green, Blue, Yellow,
@@ -18,10 +17,34 @@ interface Card {
   color: Color;
 }
 
+
 // todo move to event
 export interface GameStarted {
   kind: 'GameStarted';
+  numberOfPlayer: number;
+  firstCard: Card;
 }
+
+export interface CardPlayed {
+  kind: 'CardPlayed';
+  card: Card;
+  player: number;
+}
+
+export interface InvalidCardPlayed {
+  kind: 'InvalidCardPlayed',
+  card: Card;
+  player: number;
+}
+
+export interface NotPlayerTurn {
+  kind: 'NotPlayerTurn'; // we keep the card because this is strategic information
+  card: Card;
+  player: number;
+}
+
+export type UnoEvent =
+  GameStarted | CardPlayed | InvalidCardPlayed | NotPlayerTurn;
 
 // todo move to command
 export interface StartGame {
@@ -30,30 +53,81 @@ export interface StartGame {
   firstCard: Card;
 }
 
-// state
-export class InitinalState {
+export interface PlayCard {
+  kind: 'PlayCard';
+  card: Card;
+  player: number;
 }
 
+export type UnoCommand =
+  StartGame | PlayCard;
 
-export type UnoEvent =
-  GameStarted;
+// state
+interface InitialState {
+  kind: 'InitialState';
+}
 
-export type UnoCommand = StartGame;
+export interface Started {
+  kind: 'Started';
+  numberOfPlayer: number;
+  topCard: Card;
+  nextPlayer: number;
+}
 
-export type State = InitinalState;
-
+export type State = InitialState | Started;
 
 export const decide = curry((command: UnoCommand, state: State): UnoEvent[] => {
-  switch (command.kind) {
-    case ('StartGame'):
-      return ([{
-        kind: 'GameStarted',
-      }]);
-    default:
-      return [];
+  if (command.kind === 'StartGame' && state.kind === 'InitialState') {
+    return ([{
+      kind: 'GameStarted',
+      numberOfPlayer: command.numberOfPlayer,
+      firstCard: command.firstCard,
+    }]);
   }
+  if (command.kind === 'PlayCard' && state.kind === 'Started') {
+    if (command.player !== state.nextPlayer) {
+      return [{
+        kind: 'NotPlayerTurn',
+        card: command.card,
+        player: command.player,
+      }];
+    }
+    if (
+      (command.card.color === state.topCard.color) ||
+      (command.card.digit === state.topCard.digit)
+    ) {
+      return [{
+        kind: 'CardPlayed',
+        card: command.card,
+        player: command.player,
+      }];
+    } else {
+      return [{
+        kind: 'InvalidCardPlayed',
+        card: command.card,
+        player: command.player,
+      }];
+    }
+  }
+  if (command.kind === 'StartGame' && state.kind === 'Started') {
+    throw Error('Unexpected State');
+  }
+  return [];
 });
 
-export const evolve = curry((state: State, event: UnoEvent) => state);
-
-
+export const evolve = curry((state: State, event: UnoEvent) => {
+  switch (event.kind) {
+    case 'GameStarted':
+      return {
+        kind: 'Started',
+        numberOfPlayer: event.numberOfPlayer,
+        topCard: event.firstCard,
+        nextPlayer: 1, // we don't have guard on State Type
+      };
+    case 'CardPlayed':
+      return Object.assign(state, { topCard: event.card, nextPlayer: event.player + 1 });
+    case 'InvalidCardPlayed': // don't need this  bc no state change
+    default:
+      return state;
+  }
+});
