cae0704dbe267f91f857fd9d4e5314b819a47317
Pierre Marot <marot.pierre@gmail.com>
2017-01-31 11:39:57 +0100

    add color and digit

---
+++ src/ddd/uno/domain/Domain.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by krazar on 31/01/2017.
+ */
+
+
+enum Color {
+  Red, Green, Blue, Yellow,
+}
+
+
+enum Digit {
+  Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine
+}
+
+
+
+enum Card {
+  Dig
+}
